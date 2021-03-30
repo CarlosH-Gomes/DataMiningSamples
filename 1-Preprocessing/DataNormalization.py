a4500b009227499bf31784b80103840db0cfc692
Carlos Henrique Gomes <45035393+CarlosH-Gomes@users.noreply.github.com>
2021-03-30 20:30:42 -0300

    normalização e redução

---
+++ 1-Preprocessing/DataNormalization.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
+
+
+def main():
+    # Faz a leitura do arquivo
+    names =['date','quarter','department','day','team','targeted_productivity','smv','wip','over_time','incentive','idle_time','idle_men','no_of_style_change','no_of_workers','actual_productivity'] # Nome das colunas 
+    features  = ['targeted_productivity','smv','wip','over_time','incentive','idle_time'] # Define as colunas que serão  utilizadas
+    input_file = '0-Datasets/garments_worker_productivityClear.data'
+    target = 'team'
+    df = pd.read_csv(input_file,    # Nome do arquivo com dados
+                     names = names) # Nome das colunas                      
+    ShowInformationDataFrame(df,"Dataframe original")
+
+    # Separating out the features
+    x = df.loc[:, features].values
+    
+    # Separating out the target
+    y = df.loc[:,[target]].values
+
+    # Z-score normalization
+    x_zcore = StandardScaler().fit_transform(x)
+    normalized1Df = pd.DataFrame(data = x_zcore, columns = features)
+    normalized1Df = pd.concat([normalized1Df, df[[target]]], axis = 1)
+    ShowInformationDataFrame(normalized1Df,"Dataframe Z-Score Normalized")
+
+    # Mix-Max normalization
+    x_minmax = MinMaxScaler().fit_transform(x)
+    normalized2Df = pd.DataFrame(data = x_minmax, columns = features)
+    normalized2Df = pd.concat([normalized2Df, df[[target]]], axis = 1)
+    ShowInformationDataFrame(normalized2Df,"Dataframe Min-Max Normalized")
+
+
+def ShowInformationDataFrame(df, message=""):
+    print(message+"\n")
+    print(df.info())
+    print(df.describe())
+    print(df.head(10))
+    print("\n")  
+
+
+if __name__ == "__main__":
+    main()
